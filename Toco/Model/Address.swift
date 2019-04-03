7158266dfe8523346df71e122dc16889f8ea76ab
rolandtshen <rolandtshen@gmail.com>
2019-04-02 21:19:44 -0700

    Initial setup

---
+++ Toco/Model/Address.swift
@@ -0,0 +1,13 @@
+//
+//  Address.swift
+//  Toco
+//
+//  Created by Roland Shen on 4/2/19.
+//  Copyright © 2019 Toco. All rights reserved.
+//
+
+import Foundation
+
+class Address {
+    
+}
---
+++ Toco/Model/Client.swift
@@ -0,0 +1,16 @@
+//
+//  Client.swift
+//  Toco
+//
+//  Created by Roland Shen on 4/2/19.
+//  Copyright © 2019 Toco. All rights reserved.
+//
+
+import Foundation
+
+class Client {
+    var name: String?
+    var address: Address?
+    var phone: String?
+    var notes: String?
+}
