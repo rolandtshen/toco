0bc25dbe56a4b49b1fd1721ddf3b252c3e18f0f7
Pearl <imnotateapot@gmail.com>
2019-04-06 20:20:16 -0700

    Set Up MapVC

--- Toco/Model/Address.swift
+++ Toco/Model/Address.swift
@@ -9,5 +9,23 @@
 import Foundation
 
 class Address {
+    var addressLine1:String = ""
+    var addressLine2:String = ""
+    var city:String = ""
+    var state:String = ""
+    var country:String = ""
+    var postalCode:String = ""
+    var longitude:CLLocationDegrees = 0
+    var latitude:CLLocationDegrees = 0
     
+    init() {
+        
+    }
+    
+    init(addressLine1: String, addressLine2:String, city:String, state:String, country:String, postalCode:String, longitude:String, latitude:String) {
+    }
+    
+    func getCoordinates() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
---
+++ Toco/ViewController/MapVC.swift
@@ -0,0 +1,31 @@
+//
+//  MapVC.swift
+//  Toco
+//
+//  Created by Pearl on 3/4/2562 BE.
+//  Copyright © 2562 Toco. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapVC: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var clients:[Client] = [Client]()
+    
+    override func viewDidLoad() {
+        
+        for client:Client in clients {
+            var point = MKPointAnnotation()
+            point.title = client.getName()
+            point.coordinate = client.getAddress().getCoordinates()
+            map.addAnnotation(point)
+        }
+        
+        var region = MKCoordinateRegionMake(CLLocationCoordinate2D(latitude: 34.043627, longitude: -118.300673), MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+        map.setRegion(region, animated: true)
+    }
+ 
+}
